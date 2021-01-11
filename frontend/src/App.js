89bdd54492470f6dfd6e331d19c89d5bc2b73f2d
jimxliu <xinzhou.jim.liu@gmail.com>
2021-01-11 00:20:24 -0600

    Add initial list and submit functions in the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,78 @@
 import React, { Component } from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, Row, Table } from 'react-bootstrap';
+import axios from 'axios';
 
 class App extends Component  {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: '', // Input value
+      invalid: false, // Input is invalid
+      submissions: []
+    };
 
+    this.listSubmissions = this.listSubmissions.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.listSubmissions();
+    var intervalId = setInterval(this.listSubmissions, 15000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  listSubmissions() {
+    // GET /api/list_submissions
+    axios.get('http://localhost:8080/api/list_submissions')
+      .then(function(res){
+        console.log(res);
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+      
+    var submissions = [
+      {
+        timestamp: Date.now(),
+        result: 'No match',
+        query: 'ATGCGGGCCCCCAAAAATTTTT'
+      }
+    ];
+    this.setState({ submissions });
+  }
+
   handleChange(event) {
     this.setState({value: event.target.value});
+    // Validate
+    if(event.target.value && !this.state.value.match(/^[ATGC]+$/i)){
+      this.setState({ invalid: true});
+    } else {
+      this.setState({ invalid: false});
+    }
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
     event.preventDefault();
+    if(!this.state.value || this.state.invalid){
+      return;
+    }
+    var submission = {
+      timestamp: Date.now(),
+      result: 'Pending',
+      query: this.state.value.toLocaleUpperCase
+    }
+    this.state.submissions.push(submission);
+    this.setState({ 
+      value: '', 
+      invalid: false, 
+      submissions:  this.state.submissions 
+    });
+    // POST /api/submit
   }
 
   render() {
@@ -33,14 +89,40 @@ class App extends Component  {
                   rows={3}
                   value={this.state.value}
                   onChange={this.handleChange}
+                  isInvalid={this.state.invalid}
                 />
+                <Form.Control.Feedback type="invalid">Invalid DNA Sequence (Only accept A, T, C, G)</Form.Control.Feedback>
               </Form.Group>
               <Button type="submit">Submit</Button>
             </Form>
           </Col>
         </Row>
+        <br/>
         <Row>
-          
+          <Col>
+            <Table>
+              <thead>
+                <tr>
+                  <th style={{width: '20%'}}>Time</th>
+                  <th style={{width: '50%'}}>Results</th>
+                  <th style={{width: '30%'}}>Query</th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  this.state.submissions.map((entry, index) => {
+                    return (
+                      <tr key={ index }>
+                        <td style={{width: '20%'}}>{ entry.timestamp }</td>
+                        <td style={{width: '50%'}}>{ entry.result} </td>
+                        <td style={{width: '30%'}}>{ entry.query }</td>
+                      </tr>
+                    );
+                  })
+                }
+              </tbody>
+            </Table>
+          </Col>
         </Row>
       </Container>
     );
